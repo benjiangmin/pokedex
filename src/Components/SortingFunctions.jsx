fe505ca66ce65459c1d091e35a8593e7581bbff0
benjamin jiang <mercedesbenz5519@gmail.com>
2026-01-07 23:21:28 -0600

    adding more sorting functions

--- src/Components/SortingFunctions.jsx
+++ src/Components/SortingFunctions.jsx
@@ -20,10 +20,65 @@ export default function SortingFunctions(props) {
     })
   }
 
+  const sortBySATK = () => {
+    const sorted = [...props.results].sort((a, b) => {
+      return b.base["Special Attack"] - a.base["Special Attack"]
+    })
+    props.setResults([])
+    setTimeout(() => {
+      props.setResults(sorted)
+    })
+  }
+
+  const sortByDEF = () => {
+    const sorted = [...props.results].sort((a, b) => {
+      return b.base.Defense - a.base.Defense
+    })
+    props.setResults([])
+    setTimeout(() => {
+      props.setResults(sorted)
+    })
+  }
+
+  const sortBySDEF = () => {
+    const sorted = [...props.results].sort((a, b) => {
+      return b.base["Special Defense"] - a.base["Special Defense"]
+    })
+    props.setResults([])
+    setTimeout(() => {
+      props.setResults(sorted)
+    })
+  }
+
+  const sortBySPD = () => {
+    const sorted = [...props.results].sort((a, b) => {
+      return b.base.Speed - a.base.Speed
+    })
+    props.setResults([])
+    setTimeout(() => {
+      props.setResults(sorted)
+    })
+  }
+
+    const sortByID = () => {
+    const sorted = [...props.results].sort((a, b) => {
+      return a.id - b.id
+    })
+    props.setResults([])
+    setTimeout(() => {
+      props.setResults(sorted)
+    })
+  }
+
   return (
     <>
+      <button onClick={sortByID}>sort by #</button>
       <button onClick={sortByHP}>sort by hp</button>
       <button onClick={sortByATK}>sort by attack</button>
+      <button onClick={sortBySATK}>sort by s.attack</button>
+      <button onClick={sortByDEF}>sort by defense</button>
+      <button onClick={sortBySDEF}>sort by s.defense</button>
+      <button onClick={sortBySPD}>sort by speed</button>
     </>
   )
 }
