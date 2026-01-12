6cf6424083ae44725ed8aa169ae21461c9080ee6
benjamin jiang <mercedesbenz5519@gmail.com>
2026-01-12 12:15:03 -0600

    added dynamic stats bar

--- src/Components/PageComponents/DetailedStatsBar.jsx
+++ src/Components/PageComponents/DetailedStatsBar.jsx
@@ -1,7 +1,23 @@
 export default function DetailedStatsBar({ stats }) {
+    const maxStat = 255
+
     return (
-        <>
-            <p>HP: {stats.HP}</p>
-        </>
+        <section className="statbars-container">
+            {Object.entries(stats).map(([name, stat]) => {
+                const percentage = (stat/maxStat)*100
+
+                return (
+                    <div key={name} className="statbars-row">
+                        <div className="statbar-label">
+                            <h3>{name}</h3>
+                            <p>{stat}</p>
+                        </div>
+                        <div className="bar-background">
+                            <div className="bar-fill" style={{width: `${percentage}%`}}></div>
+                        </div>
+                    </div>
+                )
+            })}
+        </section>
     )
 }
