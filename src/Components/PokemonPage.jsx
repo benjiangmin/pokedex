72e1503256fd69af90b79d82c33f17d79987bca3
benjamin jiang <mercedesbenz5519@gmail.com>
2026-01-11 23:23:13 -0600

    started work on 3d models!

--- src/Components/PokemonPage.jsx
+++ src/Components/PokemonPage.jsx
@@ -1,22 +1,35 @@
-import { useParams, useNavigate } from "react-router-dom"
+import { useParams } from "react-router-dom"
+import { useEffect, Suspense } from "react"
+import { Canvas } from "@react-three/fiber"
+import { useGLTF, OrbitControls, Stage } from "@react-three/drei"
+
 import pokemonData from "../../pokedex-enriched.json"
-import { useEffect } from "react"
+
+function Model({ url }) {
+    const { scene } = useGLTF(url)
+    return <primitive object={scene} />
+}
 
 export default function PokemonPage() {
     const { slug } = useParams()
-    const navigate = useNavigate()
-
     const pokemon = pokemonData.find(pokemon => pokemon.slug === slug)
 
     useEffect(() => {
         window.scrollTo(0, 0)
     }, [])
 
-    const moves = pokemon.moves.map(move => (
-        <p>{move}</p>
-    ))
-    
+    const modelUrl = `https://raw.githubusercontent.com/Sudhanshu-Ambastha/Pokemon-3D-api/main/models/glb/regular/${pokemon.id}.glb`
+
     return (
-        <p>{moves}</p>
+        <div style={{ height: "500px", width: "100%" }}>
+            <Canvas shadows camera={{ position: [0, 0, 5], fov: 50 }}>
+                <Suspense fallback={null}>
+                    <Stage environment="city" intensity={0.6}>
+                        <Model url={modelUrl} />
+                    </Stage>
+                </Suspense>
+                <OrbitControls autoRotate={false} />
+            </Canvas>
+        </div>
     )
 }
