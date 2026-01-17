4ea1a8f92b2d162f1512bc95b2f1e346e31c84a3
benjamin jiang <mercedesbenz5519@gmail.com>
2026-01-17 14:55:31 -0600

    cleaned up sorting buttons

--- src/Components/SortingFunctions.jsx
+++ src/Components/SortingFunctions.jsx
@@ -1,88 +1,45 @@
 export default function SortingFunctions(props) {
-  const sortByHP = () => {
-    props.setCurrentFilter("HP")
+  const statMap = {
+    HP: "HP",
+    ATK: "Attack",
+    SATK: "Special Attack",
+    DEF: "Defense",
+    SDEF: "Special Defense",
+    SPD: "Speed"
+  };
 
-    const sorted = [...props.results].sort((a, b) => {
-      return b.base.HP - a.base.HP
-    })
-    props.setResults([])
+  const handleSort = (filterKey) => {
+    props.setCurrentFilter(filterKey);
+    const dataKey = statMap[filterKey];
+
+    const sorted = [...props.results].sort((a, b) => b.base[dataKey] - a.base[dataKey]);
+
+    props.setResults([]); 
     setTimeout(() => {
-      props.setResults(sorted)
-    })
-  }
-
-  const sortByATK = () => {
-    props.setCurrentFilter("ATK")
-
-    const sorted = [...props.results].sort((a, b) => {
-      return b.base.Attack - a.base.Attack
-    })
-    props.setResults([])
-    setTimeout(() => {
-      props.setResults(sorted)
-    })
-  }
-
-  const sortBySATK = () => {
-    props.setCurrentFilter("SATK")
-
-    const sorted = [...props.results].sort((a, b) => {
-      return b.base["Special Attack"] - a.base["Special Attack"]
-    })
-    props.setResults([])
-    setTimeout(() => {
-      props.setResults(sorted)
-    })
-  }
-
-  const sortByDEF = () => {
-    props.setCurrentFilter("DEF")
-
-    const sorted = [...props.results].sort((a, b) => {
-      return b.base.Defense - a.base.Defense
-    })
-    props.setResults([])
-    setTimeout(() => {
-      props.setResults(sorted)
-    })
-  }
-
-  const sortBySDEF = () => {
-    props.setCurrentFilter("SDEF")
-
-    const sorted = [...props.results].sort((a, b) => {
-      return b.base["Special Defense"] - a.base["Special Defense"]
-    })
-    props.setResults([])
-    setTimeout(() => {
-      props.setResults(sorted)
-    })
-  }
-
-  const sortBySPD = () => {
-    props.setCurrentFilter("SPD")
-
-    const sorted = [...props.results].sort((a, b) => {
-      return b.base.Speed - a.base.Speed
-    })
-    props.setResults([])
-    setTimeout(() => {
-      props.setResults(sorted)
-    })
-  }
+      props.setResults(sorted); 
+    }, 0); 
+  };
 
   return (
-    <>
-      <section style={{ display: "flex", justifyContent: "center" }}>
-        <section className="sorting-buttons-container">
-          <button className={props.currentFilter === "HP" ? "current-filter" : ""} onClick={sortByHP}>sort by HP</button>
-          <button className={props.currentFilter === "ATK" ? "current-filter" : ""} onClick={sortByATK}>sort by ATTACK</button>
-          <button className={props.currentFilter === "SATK" ? "current-filter" : ""} onClick={sortBySATK}>sort by SP.ATTACK</button>
-          <button className={props.currentFilter === "DEF" ? "current-filter" : ""} onClick={sortByDEF}>sort by DEFENSE</button>
-          <button className={props.currentFilter === "SDEF" ? "current-filter" : ""} onClick={sortBySDEF}>sort by SP.DEFENSE</button>
-          <button className={props.currentFilter === "SPD" ? "current-filter" : ""} onClick={sortBySPD}>sort by SPEED</button>
-        </section>
+    <section style={{ display: "flex", justifyContent: "center" }}>
+      <section className="sorting-buttons-container">
+        {Object.keys(statMap).map((key) => (
+          <button
+            key={key}
+
+            className={props.currentFilter === key ? "current-filter" : ""}
+            onClick={() => handleSort(key)}
+          >
+            sort by {
+              key === "SATK" ? "SP.ATTACK" : 
+              key === "SDEF" ? "SP.DEFENSE" : 
+              key === "ATK" ? "ATTACK" : 
+              key === "DEF" ? "DEFENSE" : 
+              key === "SPD" ? "SPEED" : key
+            }
+          </button>
+        ))}
       </section>
-    </>
-  )
+    </section>
+  );
 }
